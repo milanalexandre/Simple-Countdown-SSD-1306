657152dc7364ea30cd7de21cab566830facd5643
Alex <Alex>
2024-04-14 15:57:46 +0200

    changes delay management + improves display

--- Countdown_SSD1306.lnk.ino
+++ Countdown_SSD1306.lnk.ino
@@ -12,15 +12,16 @@
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
-//Variables declaration Hour(hora) Minute(minu) Second(sec)
-int hora = 53;
-int minu = 57;
-int sec = 60;
+// Variables declaration Hour(hours) minutete(minute) Second(sec)
+int hours = 53;
+int minute = 57;
+int sec = 59;
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
+int next_update; //save time for next display update 
 
 void setup() {
   // put your setup code here, to run once:
@@ -28,56 +29,94 @@ void setup() {
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // Address 0x3C for 128x64
   display.fillScreen(0);
   display.display();
+  next_update = millis();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  do {
-    display.clearDisplay(); //Used to clear the display
-    //Text stuff, Size and Color
-    display.setTextSize(2);
-    display.setTextColor(WHITE);
-    display.setCursor(13,0); //setCursor(x,y) //Change to your needs
-    display.print("Countdown");
-    display.setCursor(25,15);
-    display.setCursor(13,40);
-    display.setTextSize(2);
-    //This if(variable < 10) ensures that if the number is below 2 digits, it will remain centered otherwise justs prints the variable
-    if( hora < 10) {
-      display.print("0");
-      display.setCursor(25,40);
-      display.print(hora);
+void print_digits(int time_to_print)
+{
+  //This if(variable < 10) ensures that if the number is below 2 digits, it will remain centered otherwise justs prints the variable
+  if (time_to_print < 10)
+  {
+    display.print("0" + String(time_to_print));
+  }
+  else
+  {
+    display.print(time_to_print);
+  }
+}
+
+#define TextSize 3
+#define Text_X_countdown 0 //setCursor(x) //Change to your needs
+#define Text_Y_countdown 27 //setCursor(y) //Change to your needs
+
+
+void print_countdown()
+{
+  display.clearDisplay(); // Used to clear the display
+  //Text stuff, Size and Color
+  display.setTextSize(TextSize);
+  display.setTextColor(WHITE);
+  
+  // set text size
+  int16_t x1, y1;
+  uint16_t w, h; // text size
+  display.getTextBounds("0", 0, 0, &x1, &y1, &w, &h);
+  uint16_t cursor = Text_X_countdown;
+  display.setCursor(cursor, Text_Y_countdown);
+
+  // hours
+   print_digits(hours);
+  cursor = cursor + w + ((2 * w) / 3);
+  display.setCursor(cursor, Text_Y_countdown);
+
+  // split
+  display.print(":");
+  cursor = cursor + ((2 * w) / 3);
+  display.setCursor(cursor, Text_Y_countdown);
+
+  // hours
+  print_digits(minute);
+  cursor = cursor + w + ((2 * w) / 3);
+  display.setCursor(cursor, Text_Y_countdown);
+
+  // split
+  display.print(":");
+  cursor = cursor + ((2 * w) / 3);
+  display.setCursor(cursor, Text_Y_countdown);
+
+  // sec
+  print_digits(sec);
+
+  display.display(); // Display the corrent countdown
+}
+
+void remove_1sec_countdown()
+{
+  //The maths behinf this, if sec==0 then minute=minute-1 and if minute==0 then hours=hours-1, just the basics of a clock
+  sec = sec - 1;
+  if (sec == -1)
+  {
+    sec = 59;
+    minute = minute - 1;
+  }
+  if (minute == -1)
+  {
+    minute = 59;
+    hours = hours - 1;
+  }
+}
+
+void loop()
+{
+  
+  if (sec == 0 && minute == 0 && hours == 0){
+    while (true)
+    {
     }
-    else {
-      display.print(hora);
-    }
-    display.setCursor(34,40);
-    display.print(":");
-    display.setCursor(41,40);
-    if( minu < 10) {
-      display.print("0");
-      display.setCursor(53,40);
-      display.print(minu);
-    }
-    else {
-      display.print(minu);
-    }
-    display.setCursor(62,40);
-    display.print(":");
-    display.setCursor(70,40);
-    display.print(sec);
-    //Delay for 1s = 1000ms
-    delay(1000);
-    //The maths behinf this, if sec==0 then minu=minu-1 and if minu==0 then hora=hora-1, just the basics of a clock
-    sec = sec - 1;
-    if(sec == 0) {
-      sec = 60;
-      minu = minu - 1;
-    }
-    if (minu < 0) {
-      minu = 60;
-      hora = hora - 1;
-    }
-    display.display(); //Display the corrent countdown
-  } while (hora != 0);
+  }
+  if (next_update < millis()){
+    next_update += 1000;
+    remove_1sec_countdown();
+    print_countdown();
+  }
 }
